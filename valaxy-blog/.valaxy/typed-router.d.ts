959d98e8e4718ad4ac8da227f2c88773f8f606e7
XIARUI100415 <1948083823@qq.com>
2025-05-10 02:17:50 +0800

    1

--- valaxy-blog/.valaxy/typed-router.d.ts
+++ valaxy-blog/.valaxy/typed-router.d.ts
@@ -29,7 +29,7 @@ declare module 'vue-router/auto-routes' {
     '/links/': RouteRecordInfo<'/links/', '/links', Record<never, never>, Record<never, never>>,
     '/page/[page]': RouteRecordInfo<'/page/[page]', '/page/:page', { page: ParamValue<true> }, { page: ParamValue<false> }>,
     '/posts/': RouteRecordInfo<'/posts/', '/posts', Record<never, never>, Record<never, never>>,
-    '/posts/hello-valaxy': RouteRecordInfo<'/posts/hello-valaxy', '/posts/hello-valaxy', Record<never, never>, Record<never, never>>,
+    '/posts/随笔——致李燕春': RouteRecordInfo<'/posts/随笔——致李燕春', '/posts/随笔——致李燕春', Record<never, never>, Record<never, never>>,
     '/tags/': RouteRecordInfo<'/tags/', '/tags', Record<never, never>, Record<never, never>>,
   }
 }
